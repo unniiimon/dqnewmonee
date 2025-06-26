585644ba4ba16d1cbd43ed2b87bea69d29e67c1e
unniiimon <unniiimon123@gmail.com>
2025-06-26 10:31:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1265,28 +1265,29 @@ async def auto_filter(client, msg, spoll=False):
         total_results = len(files)
         pre = 'filep' if settings['file_secure'] else 'file'
     if settings["button"]:
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"🔹[{get_size(file.file_size)}]🔸{file.file_name}", callback_data=f'{pre}#{file.file_id}'
-                ),
-            ]
-            for file in files
+    btn = [
+        [
+            InlineKeyboardButton(
+                text=f"🔹[{get_size(file['file_size'])}]🔸{file['file_name']}",
+                callback_data=f'{pre}#{file["file_id"]}'
+            )
         ]
-    else:
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"🔹{file.file_name}",
-                    callback_data=f'{pre}#{file.file_id}',
-                ),
-                InlineKeyboardButton(
-                    text=f"🔸{get_size(file.file_size)}",
-                    callback_data=f'{pre}#{file.file_id}',
-                ),
-            ]
-            for file in files
+        for file in files
+    ]
+else:
+    btn = [
+        [
+            InlineKeyboardButton(
+                text=f"🔹{file['file_name']}",
+                callback_data=f'{pre}#{file["file_id"]}',
+            ),
+            InlineKeyboardButton(
+                text=f"🔸{get_size(file['file_size'])}",
+                callback_data=f'{pre}#{file["file_id"]}',
+            ),
         ]
+        for file in files
+    ]
 
     try:
         if settings['auto_delete']:
