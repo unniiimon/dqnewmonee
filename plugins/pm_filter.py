d791ca9e536daa8b7b8dc444545aae4a32a8e55d
unniiimon <unniiimon123@gmail.com>
2025-06-26 08:30:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1260,7 +1260,7 @@ async def auto_filter(client, msg, spoll=False):
             return
     else:
         settings = await get_settings(msg.chat.id)
-        message = msg.message.reply_to_message  # msg will be callback query
+        message = msg.reply_to_message # msg will be callback query
         search, files, offset, total_results = spoll
     pre = 'filep' if settings['file_secure'] else 'file'
     if settings["button"]:
